71323df2c772e7aa8b403bbe5fcd5bf716dec5c8
Letícia Monteiro <monteiro.leticia@gmail.com>
2026-04-01 00:38:50 -0300

    chore: inicializar monorepo com npm workspaces e Turborepo

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y'],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    'react/prop-types': 'off',
+  },
+  ignorePatterns: ['dist', 'node_modules', 'storybook-static', 'coverage'],
+};
---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: [
+    '../packages/**/src/**/*.stories.@(ts|tsx)',
+    '../stories/**/*.stories.@(ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
+
+export default config;
